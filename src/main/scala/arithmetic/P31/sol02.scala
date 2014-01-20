b19f7f887bbb383c061b70fc57f6e02d630cfd73
Guillaume Dubuisson Duplessis <guillaume.dubuisson_duplessis@insa-rouen.fr>
2014-01-20 15:19:34 +0100

    License info

--- src/main/scala/arithmetic/P31/sol02.scala
+++ src/main/scala/arithmetic/P31/sol02.scala
@@ -1,6 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Guillaume DUBUISSON DUPLESSIS <guillaume.dubuisson_duplessis@insa-rouen.fr>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Guillaume DUBUISSON DUPLESSIS <guillaume.dubuisson_duplessis@insa-rouen.fr> - initial API and implementation
+ ******************************************************************************/
 package arithmetic.P31
-
-import org.scalatest.Args
 
 class sol02 extends P31 {
   def isPrime(n: Int): Boolean =
