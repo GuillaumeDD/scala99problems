4a4aaaf53815e45e93e4c2185e959ff20b13d243
Guillaume Dubuisson Duplessis <guillaume.dubuisson_duplessis@insa-rouen.fr>
2014-01-23 10:54:23 +0100

    P32: move gcd into the companion object in order to be usable in other problems

--- src/main/scala/arithmetic/P32/sol01.scala
+++ src/main/scala/arithmetic/P32/sol01.scala
@@ -1,19 +1,26 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * Copyright (c) 2014 Guillaume DUBUISSON DUPLESSIS <guillaume.dubuisson_duplessis@insa-rouen.fr>.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
+ *
  * Contributors:
  *     Guillaume DUBUISSON DUPLESSIS <guillaume.dubuisson_duplessis@insa-rouen.fr> - initial API and implementation
- ******************************************************************************/
+ * ****************************************************************************
+ */
 package arithmetic.P32
 
 import org.scalatest.Args
 
 class sol01 extends P32 {
 
+  def gcd(a: Int, b: Int): Int =
+    sol01.gcd(a, b)
+}
+
+object sol01 {
   def gcd(a: Int, b: Int): Int =
     {
       require(a > 0 || b > 0)
