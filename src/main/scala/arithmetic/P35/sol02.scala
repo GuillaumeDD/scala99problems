339b1235dfb1d77c11b29074d98e08c7e45c6811
Guillaume Dubuisson Duplessis <guillaume.dubuisson_duplessis@insa-rouen.fr>
2014-01-25 16:34:48 +0100

    P35: move primeFactors into the companion object in order to be usable in other problems

--- src/main/scala/arithmetic/P35/sol02.scala
+++ src/main/scala/arithmetic/P35/sol02.scala
@@ -1,13 +1,15 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * Copyright (c) 2014 Guillaume DUBUISSON DUPLESSIS <guillaume.dubuisson_duplessis@insa-rouen.fr>.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
+ *
  * Contributors:
  *     Guillaume DUBUISSON DUPLESSIS <guillaume.dubuisson_duplessis@insa-rouen.fr> - initial API and implementation
- ******************************************************************************/
+ * ****************************************************************************
+ */
 package arithmetic.P35
 
 import org.scalatest.Args
@@ -15,6 +17,11 @@ import scala.collection.immutable.List
 import scala.annotation.tailrec
 
 class sol02 extends P35 {
+  def primeFactors(n: Int): List[Int] =
+    sol02.primeFactors(n)
+}
+
+object sol02 {
   def primeFactors(n: Int): List[Int] =
     {
       require(n > 1)
