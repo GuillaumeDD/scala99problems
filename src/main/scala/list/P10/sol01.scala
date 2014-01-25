110c5b1d5497448bad80fda637b746ae28fb0ceb
Guillaume Dubuisson Duplessis <guillaume.dubuisson_duplessis@insa-rouen.fr>
2014-01-25 16:35:32 +0100

    P10: move encode into the companion object in order to be usable in other problems

--- src/main/scala/list/P10/sol01.scala
+++ src/main/scala/list/P10/sol01.scala
@@ -1,16 +1,23 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * Copyright (c) 2013 Guillaume DUBUISSON DUPLESSIS <guillaume.dubuisson_duplessis@insa-rouen.fr>.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
+ *
  * Contributors:
  *     Guillaume DUBUISSON DUPLESSIS <guillaume.dubuisson_duplessis@insa-rouen.fr> - initial API and implementation
- ******************************************************************************/
+ * ****************************************************************************
+ */
 package list.P10
 
 class sol01 extends P10 {
+  def encode[T](l: List[T]): List[(Int, T)] =
+    sol01.encode(l)
+}
+
+object sol01 {
   import list.P09.sol01._
   def encode[T](l: List[T]): List[(Int, T)] =
     l match {
