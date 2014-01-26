89e37af34db8402e2ac9e2304d23d0b3de5d65da
Guillaume Dubuisson Duplessis <guillaume.dubuisson_duplessis@insa-rouen.fr>
2014-01-26 21:09:52 +0100

    Tictac procedure to benchmark some algorithms

--- src/main/scala/util/package.scala
+++ src/main/scala/util/package.scala
@@ -18,4 +18,34 @@ package object util {
    */
   def randomInt(borneSup: Int): Int =
     scala.util.Random.nextInt(borneSup)
+
+  // Modified from: http://stackoverflow.com/questions/9160001/how-to-profile-methods-in-scala
+  def tictac[R](label: String, repetition: Int = 10000)(block: => R): Double = {
+    require(repetition > 1)
+    val computedTimes =
+      for (i <- 1 to repetition)
+        yield ({
+        val t0 = System.nanoTime()
+        val result = block // call-by-name
+        val t1 = System.nanoTime()
+        t1 - t0
+      })
+
+    // Drop the first computation of time (which is empirically often aberrant)
+    val times = computedTimes.tail
+    val sizeOfTimes = repetition - 1
+    val avgTime = times.sum.toDouble / sizeOfTimes
+    val standardDeviation = Math.sqrt(variance(times.map(_ / 1000), avgTime / 1000))
+    // Uncomment to display the series of measure (sorted and associated with their index)
+    // println(times.map(_ / 1000).zip(1 to sizeOfTimes).sortBy(x => x._1).mkString(", "))
+    println(f"$label | Elapsed time: ${avgTime / 1000}%4.2fµs (standard deviation: $standardDeviation%4.2f)")
+    avgTime
+  }
+
+  private def variance(seq: IndexedSeq[Long], avg: Double): Double =
+    {
+      require(seq.size > 1)
+      seq.foldLeft(0.0)((acc, elt) => acc + Math.pow((avg - elt), 2)) / (seq.size - 1)
+    }
+
 }
