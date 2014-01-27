8d6282620b26294b68fa8fc310731c63642199c3
Guillaume Dubuisson Duplessis <guillaume.dubuisson_duplessis@insa-rouen.fr>
2014-01-27 12:01:20 +0100

    P31: move isPrime into the companion object in order to be usable in other problems

--- src/main/scala/arithmetic/P31/sol02.scala
+++ src/main/scala/arithmetic/P31/sol02.scala
@@ -1,16 +1,23 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * Copyright (c) 2014 Guillaume DUBUISSON DUPLESSIS <guillaume.dubuisson_duplessis@insa-rouen.fr>.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
+ *
  * Contributors:
  *     Guillaume DUBUISSON DUPLESSIS <guillaume.dubuisson_duplessis@insa-rouen.fr> - initial API and implementation
- ******************************************************************************/
+ * ****************************************************************************
+ */
 package arithmetic.P31
 
 class sol02 extends P31 {
+  def isPrime(n: Int): Boolean =
+    sol02.isPrime(n)
+}
+
+object sol02 {
   def isPrime(n: Int): Boolean =
     {
       require(n >= 0)
