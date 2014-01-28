b48d4e36164ec20d399eef6e3e399b72eb8c9e7f
Guillaume Dubuisson Duplessis <guillaume.dubuisson_duplessis@insa-rouen.fr>
2014-01-28 10:21:53 +0100

    P36: benchmark of the different algorithms and license info

--- src/main/scala/arithmetic/P36/BenchmarkP36.scala
+++ src/main/scala/arithmetic/P36/BenchmarkP36.scala
@@ -1,3 +1,15 @@
+/**
+ * *****************************************************************************
+ * Copyright (c) 2014 Guillaume DUBUISSON DUPLESSIS <guillaume.dubuisson_duplessis@insa-rouen.fr>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Contributors:
+ *     Guillaume DUBUISSON DUPLESSIS <guillaume.dubuisson_duplessis@insa-rouen.fr> - initial API and implementation
+ * ****************************************************************************
+ */
 package arithmetic.P36
 import util.tictac
 
@@ -5,10 +17,6 @@ object BenchmarkP36 extends App {
   // Loading primes
   val primes = arithmetic.P31.sol03.primes
   // Benchmark
-  tictac("P36-sol01") {
-    (new sol01()).primeFactorMultiplicity(315)
-  }
-
   tictac("P36-sol01") {
     (new sol01()).primeFactorMultiplicity(315)
   }
